9e3b9d70a04e44d8ce055e071593759b2dc70181
iryamr <iryamuhammadr@gmail.com>
2022-01-15 00:28:50 +0700

    Add room database

---
+++ core/src/main/java/com/dicoding/made/core/data/source/local/room/MovieDao.kt
@@ -0,0 +1,34 @@
+package com.dicoding.made.core.data.source.local.room
+
+import androidx.room.*
+import com.dicoding.made.core.data.source.local.entities.MovieEntity
+import com.dicoding.made.core.data.source.local.entities.TvShowEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM movies")
+    fun getAllMovies(): Flow<List<MovieEntity>>
+
+    @Query("SELECT * FROM movies where is_favorite = 1")
+    fun getFavoriteMovies(): Flow<List<MovieEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovies(movies: List<MovieEntity>)
+
+    @Update
+    fun updateFavoriteMovie(movie: MovieEntity)
+
+    @Query("SELECT * FROM tv_shows")
+    fun getAllTvShows(): Flow<List<TvShowEntity>>
+
+    @Query("SELECT * FROM tv_shows where is_favorite = 1")
+    fun getFavoriteTvShows(): Flow<List<TvShowEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTvShows(tvShows: List<TvShowEntity>)
+
+    @Update
+    fun updateFavoriteTvShow(tvShow: TvShowEntity)
+}
---
+++ core/src/main/java/com/dicoding/made/core/data/source/local/room/MovieDatabase.kt
@@ -0,0 +1,19 @@
+package com.dicoding.made.core.data.source.local.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.dicoding.made.core.data.source.local.entities.MovieEntity
+import com.dicoding.made.core.data.source.local.entities.TvShowEntity
+
+@Database(
+    entities = [
+        MovieEntity::class,
+        TvShowEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class MovieDatabase : RoomDatabase() {
+
+    abstract fun movieDao(): MovieDao
+}
